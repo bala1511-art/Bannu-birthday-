9fd72cb2aaf0250b6396b96d40ede5c12a61c1c3
bala1511-art <db3595021@gmail.com>
2025-06-24 09:27:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-// Wait until the entire page content is loaded
 window.addEventListener("DOMContentLoaded", () => {
   const card = document.getElementById("card");
-
-  // Add the 'show' class to trigger the CSS animation
   if (card) {
     card.classList.add("show");
   }
